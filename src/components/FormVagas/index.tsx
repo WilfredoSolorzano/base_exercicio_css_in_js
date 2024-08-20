831cdd7b5a0080b602acc7765c4d3d6501ec9793
WilfredoSolorzano <wsolorzano00@gmail.com>
2024-08-20 17:41:28 -0300

    um fork para tarefa module 29

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,6 +1,5 @@
 import { FormEvent, useState } from 'react'
-
-import styles from './FormVagas.module.css'
+import { FormContainer, SearchButton, SearchInput } from './FormVagas.styles'
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -15,17 +14,15 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <FormContainer onSubmit={aoEnviarForm}>
+      <SearchInput
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
-    </form>
+      <SearchButton type="submit">Pesquisar</SearchButton>
+    </FormContainer>
   )
 }
+
 export default FormVagas
